e84c9880faed82b3084f0b0a9604bb6c4273b848
Shana <lashana.breland.gmail.com>
2016-11-16 23:59:51 -0500

    link and button changes

---
+++ script.js
@@ -0,0 +1,7 @@
+var newHireButton = document.getElementById("new");
+var contents = document.getElementsByTagName('a')
+
+newHireButton.focus = function(){
+        a = "Used for New and Returning Employees"
+
+};
